455d93ab3b6da114e702ef35bc5d9f87229961ff
Tamara <tamara.shcherbyna@get.no>
2014-10-09 15:25:27 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var connect = require('connect'),
+    serveStatic = require('serve-static');
+
+var app = connect();
+
+app.use(serveStatic("angularjs", {default: "index.html"}));
+app.listen(5000);
