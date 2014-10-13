7a6a72d1b7c3bd5d958799764ad0c1ba025a5f75
Tamara <tamara.shcherbyna@get.no>
2014-10-13 14:32:44 +0300

    add controller to app.js

--- angularjs/scripts/app.js
+++ angularjs/scripts/app.js
@@ -7,8 +7,12 @@
             { action: "Get Shoes", done: false },
             { action: "Collect Tickets", done: true },
             { action: "Call Joe", done: false }]
-    };
+        },
 
-    angular.module("todoApp", []);
+        todoApp = angular.module("todoApp", []);
+
+    todoApp.controller("ToDoCtrl", function ($scope) {
+        $scope.todo = model;
+    });
 
 })();
