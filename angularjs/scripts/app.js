41428d4590ee0b1354fc9c53c85508e93ed808d4
Tamara <tamara.shcherbyna@get.no>
2014-10-14 11:03:37 +0300

    creating and Using Controller Behavior

--- angularjs/scripts/app.js
+++ angularjs/scripts/app.js
@@ -13,6 +13,14 @@
 
     todoApp.controller("ToDoCtrl", function ($scope) {
         $scope.todo = model;
+
+        $scope.incompleteCount = function () {
+            var count = 0;
+            angular.forEach($scope.todo.items, function (item) {
+                if (!item.done) { count++ }
+            });
+            return count;
+        }
     });
 
 })();
