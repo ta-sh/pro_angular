7ac1d0f7ccd1475c7388555f1e7a3b90a726ac0e
Tamara <tamara.shcherbyna@get.no>
2014-10-14 11:09:07 +0300

    Using Behaviors That Depend on Other Behaviors

--- angularjs/scripts/app.js
+++ angularjs/scripts/app.js
@@ -21,6 +21,10 @@
             });
             return count;
         }
+
+        $scope.warningLevel = function () {
+            return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+        }
     });
 
 })();
