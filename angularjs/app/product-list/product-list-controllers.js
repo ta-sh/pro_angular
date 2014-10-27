ffe8c51c78258ea72d604df8ce4f39def01eaa39
Tamara <tamara.shcherbyna@get.no>
2014-10-27 18:47:03 +0200

    SportsStore: A Real Application

---
+++ angularjs/app/product-list/product-list-controllers.js
@@ -0,0 +1,31 @@
+angular.module("sportsStore")
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, productListActiveClass, productListPageCount) {
+        var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
+
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        };
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory === null ||
+                product.category === selectedCategory;
+        };
+
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
+        }
+
+        $scope.getCategoryClass = function (category) {
+            return selectedCategory == category ? productListActiveClass : "";
+        };
+    });
