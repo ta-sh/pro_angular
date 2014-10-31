2a9d45b256bd07e7a5706142d364edba055406cc
Tamara <tamara.shcherbyna@get.no>
2014-10-31 16:16:24 +0200

    add Cart

--- angularjs/app/app.js
+++ angularjs/app/app.js
@@ -1,6 +1,23 @@
 (function () {
     "use strict";
 
-    angular.module("sportsStore", ["customFilters"]);
+    angular.module("sportsStore", ["customFilters", "cart", "ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/complete", {
+            templateUrl: "app/components/cart/thank-you.html"
+        });
+        $routeProvider.when("/placeorder", {
+            templateUrl: "app/components/cart/place-order.html"
+        });
+        $routeProvider.when("/checkout", {
+            templateUrl: "app/components/cart/checkout-summary.html"
+        });
+        $routeProvider.when("/products", {
+            templateUrl: "app/product-list/product-list-view.html"
+        });
+        $routeProvider.otherwise({
+            templateUrl: "app/product-list/product-list-view.html"
+        });
+    });
 
 })();
--- angularjs/app/product-list/product-list-controllers.js
+++ angularjs/app/product-list/product-list-controllers.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
     .constant("productListPageCount", 3)
-    .controller("productListCtrl", function ($scope, productListActiveClass, productListPageCount) {
+    .controller("productListCtrl", function ($scope, productListActiveClass, productListPageCount, cart) {
         var selectedCategory = null;
 
         $scope.selectedPage = 1;
@@ -28,4 +28,8 @@ angular.module("sportsStore")
         $scope.getCategoryClass = function (category) {
             return selectedCategory == category ? productListActiveClass : "";
         };
+
+        $scope.addProductToCart = function (product) {
+            cart.addProduct(product.id, product.name, product.price);
+        }
     });
